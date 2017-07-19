9bee99043a46504d3cb3c1e36c408600a3f33952
Kazuki Sugita <the.kaz.subconscious.8@gmail.com>
2017-07-19 12:27:20 +0900

    update system

--- gulpfile.js
+++ gulpfile.js
@@ -10,16 +10,41 @@ var elixir = require('laravel-elixir');
  |
  */
 
+/*
+
+    ./node_modules/bootstrap/scss/bootstrap.scss
+    ./node_modules/jquery/dist/jquery.min.js
+    ./node_modules/tether/dist/js/tether.min.js
+    ./node_modules/bootstrap/dist/js/bootstrap.min.js
+
+    ./node_modules/scrollreveal/dist/scrollreveal.min.js
+    ./node_modules/@tweenjs/tween.js/src/Tween.js
+    ./node_modules/waypoints/lib/jquery.waypoints.min.js
+
+     .scripts([
+        scrollreveal,
+        Tween.js,
+        waypoints.js,
+        flickity,
+        rellax,
+     ], 'public/js/visuals.js')
+
+*/
+
 elixir(function(mix) {
     mix
         .sass([
+            './node_modules/bootstrap/scss/bootstrap.scss',
             'components.scss',
             'styles.scss'
         ], 'public/css/styles.css')
         .scripts([
+            './node_modules/jquery/dist/jquery.min.js',
+            './node_modules/tether/dist/js/tether.min.js',
+            './node_modules/bootstrap/dist/js/bootstrap.min.js',
             'angular.min.js',
             'angular-static-directives.js'
-        ], 'public/js/angular.js')
+        ], 'public/js/fundamentals.js')
         .browserSync({
             proxy: 'localhost:8000'
         });
--- resources/assets/js/angular-static-directives.js
+++ resources/assets/js/angular-static-directives.js
@@ -6,13 +6,32 @@ angular.module('body')
         return {
             restrict: 'E',
             templateUrl: '/templates/static-header.html',
-            replace: true
+            replace: true,
+            compile: function (elem, attrs) {
+                var path_ = window.location.pathname;
+                if (path_ !== '' || null || undefined) {
+                    var links = angular.element(elem).find(".nav-link");
+                    angular.forEach(links, function (link) {
+                        if (angular.element(link).attr("href") == path_) {
+                            angular.element(link).parent().addClass("active");
+                            return false;
+                        }
+                    });
+                }
+            }
         }
     })
     .directive('staticFooter', function() {
         return {
             restrict: 'E',
             templateUrl: '/templates/static-footer.html',
+            replace: true
+        }
+    })
+    .directive('dummyText', function() {
+        return {
+            restrict: 'E',
+            templateUrl: '/templates/dummy-text.html',
             replace: true
         }
     });
